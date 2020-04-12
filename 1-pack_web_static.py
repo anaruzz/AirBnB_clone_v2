0b32e2d0add40d58c2f027424d110d7c9929abd5
anaruzz <laabidigh@gmail.com>
2020-04-12 14:42:56 +0100

    ok

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     """
     local(mkdir versions)
     file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
-            .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
     compress = local("tar -cvzf" + file_name + "/web_static")
     if compress.succeeded:
         return file_name
