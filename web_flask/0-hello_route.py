bf61bfbd6ce10aa11319154750f4576819611957
anaruzz <laabidigh@gmail.com>
2020-04-27 00:07:39 +0100

    pep8 Fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
